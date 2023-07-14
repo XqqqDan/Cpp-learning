0f15eb76c8647d751a6ce452068486f85b099ed9
xqqqdan <xqqqdan@gmail.com>
2023-07-15 02:15:28 +0800

    input

---
+++ input.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <limits>
+
+int main()
+{
+    int i = 10;
+    while (std::cin >> i, !std::cin.eof())
+    {
+        if (std::cin.bad())
+        {
+            throw std::runtime_error("cin is corrupted");
+        }
+        if (std::cin.fail())
+        {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "data format error, please try again" << std::endl;
+            continue;
+        }
+        std::cout << i << std::endl;
+    }
+    std::cout << "process complete" << std::endl;
+    return 0;
+}
