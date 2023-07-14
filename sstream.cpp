e99e066210dd3fe2967d8c86f361c110c92d19a5
xqqqdan <xqqqdan@gmail.com>
2023-07-15 05:16:53 +0800

    sstream

--- sstream.cpp
+++ sstream.cpp
@@ -46,15 +46,32 @@ std::string Int_To_String() // int转化成string
         {
             throw std::runtime_error("strStream is corrupted");
         }
-        if (strStream.fail())
+        if (strStream.fail()) //?????????????
         {
             strStream.clear();
             strStream.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             std::cout << "int format error, please try again" << std::endl;
             continue;
         }
+
         std::cout << "转化完成" << std::endl;
         return strStream.str();
+    }
+}
+
+void String_To_Words()
+{
+    std::string srcString("hello world hello");
+    std::string destString;
+    // std::cin >> srcString;??????????
+    std::stringstream strStream(srcString);
+    while (strStream >> destString)
+    {
+        std::cout << destString << std::endl;
+    }
+    if (strStream.bad())
+    {
+        throw std::runtime_error("strStream is corrupted");
     }
 }
 
@@ -62,10 +79,11 @@ int main()
 {
     int input = 0;
     std::string str;
-    std::cout << "请选择转化模式:" << std::endl;
+    std::cout << "请选择模式:" << std::endl;
     std::cout << "1: string 转 int" << std::endl;
     std::cout << "2: int 转 string" << std::endl;
-    std::cout << "请输入[1/2]:" << std::endl;
+    std::cout << "3: 字符串去空格" << std::endl;
+    std::cout << "请输入[1/2/3]:" << std::endl;
 
     while (std::cin >> input, !std::cin.eof())
     {
@@ -77,12 +95,12 @@ int main()
         {
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            std::cout << "请输入[1/2]" << std::endl;
+            std::cout << "请输入[1/2/3]:" << std::endl;
             continue;
         }
-        if ((input != 1) && (input != 2))
+        if ((input != 1) && (input != 2) && (input != 3))
         {
-            std::cout << "请输入[1/2]" << std::endl;
+            std::cout << "请输入[1/2/3]:" << std::endl;
             continue;
         }
         break;
@@ -92,9 +110,14 @@ int main()
     {
         std::cout << String_To_Int() << std::endl;
     }
-    else
+    else if (input == 2)
     {
         std::cout << Int_To_String() << std::endl;
+    }
+    else
+    {
+        std::cout << "请输入文本:" << std::endl;
+        String_To_Words();
     }
     return 0;
 }
